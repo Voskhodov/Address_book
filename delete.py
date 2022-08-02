a0b9570dfd0709477e56a5c78475665fadf2bf6d
Alexandra <sashatoropova@gmail.com>
2022-08-02 23:47:14 +0700

    corrected delete 2.0

--- delete.py
+++ delete.py
@@ -1,13 +1,15 @@
+from options import database
+
 def delete():
-    data_for_del = input('Введите данные для удаления в формате: Фамилия Имя Отчество.\n'
+    data_for_del = input('Введите данные для удаления в формате: Фамилия;Имя;Отчество.\n'
                          '* При отсутсвии однофамильцев достаточно ввести фамилию ученика: ')
     del_data(data_for_del)
 
 
 def del_data(data_for_del: str):
-    with open('fio.csv', 'r', encoding="utf-8") as data:
+    with open(database, 'r', encoding="utf-8") as data:
         lines = data.readlines()
-    with open('fio.csv', 'w', encoding="utf-8") as data:
+    with open(database, 'w', encoding="utf-8") as data:
         for line in lines:
             if data_for_del not in line:
                 data.write(line)
