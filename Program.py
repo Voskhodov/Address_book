0aa16191bfdbde2a11ebcb598ffb186b89789656
gmpcrm <gmpcrm@gmail.com>
2022-08-10 17:33:18 +0200

    фреймы

--- Program.py
+++ Program.py
@@ -24,13 +24,34 @@ commands = {
 }
 
 def run_gui():
+    def do_search():
+        inputValue=textSearch.get("1.0","end-1c")
+        print(inputValue)
+
     ws = tkinter.Tk()
     ws.title('Список пользователей')
 
+    searchFrame=tkinter.Frame(ws)
+    searchFrame.pack(fill='both')
+    textSearch=tkinter.Text(searchFrame, height=1, width=100)
+    textSearch.pack(side='left', fill='both', expand='yes', padx=10, pady=5)
+    buttonSearch=tkinter.Button(searchFrame, height=1, width=10, text="Поиск", command=lambda: do_search())
+    buttonSearch.pack(side='right', padx=15, pady=5)
+
+    mainFrame=tkinter.Frame(ws)
+    mainFrame.pack(fill='both', expand='yes')
+
     df = ab_import.load()
     columns = list(df.columns)
-    tree = ttk.Treeview(ws, columns=columns, show='headings', height=5)
+    tree = ttk.Treeview(mainFrame, columns=columns, show='headings', height=10)
     tree.pack(fill='both', expand='yes')
+
+    buttonFrame=tkinter.Frame(mainFrame)
+    buttonFrame.pack(side='right')
+    buttonAdd=tkinter.Button(buttonFrame, height=1, width=10, text="Добавить", command=lambda: do_search())
+    buttonAdd.pack(side='bottom', padx=15, pady=5)
+    buttonRemove=tkinter.Button(buttonFrame, height=1, width=10, text="Удалить", command=lambda: do_search())
+    buttonRemove.pack(side='bottom', padx=15, pady=5)
 
     tree['columns'] = columns
     for col in columns:
